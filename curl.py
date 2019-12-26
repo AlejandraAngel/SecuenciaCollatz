c5d2443000f6b523d9c43fd552fa917b5020f1ad
AlejandraAngel <49563437+AlejandraAngel@users.noreply.github.com>
2019-12-26 11:22:18 -0300

    Update curl.py

--- curl.py
+++ curl.py
@@ -1,4 +1,8 @@
- import requests 
+import requests
 
- r = requests.get('https://github.com/timeline.json') 
- r.json() 
+valores = [30, 'sa',13, 20, '30r', 30, '1w', '', 125, ]
+i = 0
+
+for i in valores:
+    r = requests.get('http://localhost/calculoSecuenciaCollatz.php?numero='+str(i))
+    print(r.json()) # or r.json()
